0943a8f0dde322485bb3999959d3922746c1fa31
Neel Gore <57605901+neelgore@users.noreply.github.com>
2020-04-11 18:21:12 -0700

    refactored

--- src/sheets.py
+++ src/sheets.py
@@ -19,28 +19,42 @@ def get_data() -> (dict, [[str]]):
 
 def calculate(elos_and_results: (dict, [[str]])) -> dict:
     elos, results = elos_and_results
-    for i, game in enumerate(results):
-        total_elo = sum([elos[name][0] for name in game])
+    for game in results:
+        total_elo = sum([elos[player][0] for player in game])
         expected_scores = []
         for player in game:
             average_of_others = (total_elo - elos[player][0])/3
             expected_scores.append(1/(1 + 10**((average_of_others - elos[player][0])/400))) #Elo formula
+        #expected_scores[i] stores the expected score for game[i]
         result = lambda i: 1 if i == 0 else 0
-        elos[game[0]] = (elos[game[0]][0] + round(32*(1 - expected_scores[0])), round(32*(1 - expected_scores[0]))) #adjust winner's Elo using Elo formula
-        for i, player in enumerate(game[1:]):
-            elos[player] = (elos[player][0] + round(32*(result(i + 1) - expected_scores[i + 1])/3), round(32*(result(i + 1) - expected_scores[i + 1])/3))
-            #losers lose 1/3 of what they should lose so that Elo is roughly conserved
-        for name in elos:
-            if name not in game:
-                elos[name] = (elos[name][0], None)
+        #because data is stored so that game[0] is the winner of the game
+        elo_change = lambda result, expected: round(32*(result - expected)) if result == 1 else round(32*(result - expected)/3)
+        #losers lose 1/3 of what they should lose so that Elo is roughly conserved
+        for i, player in enumerate(game):
+            player_change = elo_change(result(i), expected_scores[i])
+            elos[player] = (elos[player][0] + player_change, player_change)
+        #adjust elos
+        for player in elos:
+            if player not in game:
+                elos[player] = (elos[player][0], 0)
+        #set change of players who weren't in this game to 0
     return elos
 
+def str_of_elo_change(change: int) -> str:
+    if change == 0:
+        return ""
+    elif change < 0:
+        return "(" + str(change) + ")"
+    else:
+        return "(+" + str(change) + ")"
+
+def print_elos(elos: dict) -> None:
+    for k, v in sorted(elos.items(), key = lambda x: x[1], reverse = True):
+        print(k, str(v[0]).rjust(15 - len(k)), str_of_elo_change(v[1]))
+    print()
+
 def run() -> None:
-    answers = calculate(get_data())
-    with_sign = lambda num: "" if num is None else "(+" + str(num) + ")" if num >= 0 else "(" + str(num) + ")"
-    for k, v in sorted(answers.items(), key = lambda x: x[1], reverse = True):
-        print(k, str(v[0]).rjust(15 - len(k)), with_sign(v[1]))
-    
+    print_elos(calculate(get_data()))
     
 if __name__ == "__main__":
     run()
